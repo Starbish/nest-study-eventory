ea39f72930e9638943b76703bf3f12acaaa6b706
Kyuhyeon Woo <starbish96@korea.ac.kr>
2024-10-05 17:27:21 +0900

    Auto stash before merge of "develop" and "origin/develop"

--- src/categories/category.controller.ts
+++ src/categories/category.controller.ts
@@ -8,11 +8,10 @@ import { CategoryListDto } from './dto/category.dto';
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
-  @ApiOkResponse({ type: CategoryListDto })
   @Get()
   @ApiOperation({ summary: '전체 카테고리를 조회합니다.' })
   @ApiOkResponse({ type: CategoryListDto })
   async findAllCategories(): Promise<CategoryListDto> {
-    return this.categoryService.findAllCategories();
+      return this.categoryService.findAllCategories();
   }
 }
