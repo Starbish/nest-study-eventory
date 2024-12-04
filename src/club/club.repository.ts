ecb70ca33679ef6bb407b0c1de5f64168a8746e6
Kyuhyeon Woo <starbish96@korea.ac.kr>
2024-12-04 14:27:11 +0900

    클럽 정보 조회 기능

---
+++ src/club/club.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ClubInfoDto } from './dto/club-info.dto';
+import { PrismaService } from 'src/common/services/prisma.service';
+import { ClubInfoData } from './type/club-info-data.type';
+
+@Injectable()
+export class ClubRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getClubInfo(clubId: number): Promise<ClubInfoData | null>{
+    return this.prisma.club.findUnique({
+      where: {
+        id: clubId,
+      }
+    })
+  }
+}
---
+++ src/club/type/club-info-data.type.ts
@@ -0,0 +1,7 @@
+export type ClubInfoData = {
+    id: number;
+    title: string;
+    description?: string | null;
+    ownerId: number;
+};
+  
